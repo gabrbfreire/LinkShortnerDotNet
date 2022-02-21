96dabedd204736c50bb5083ad33a4323c6f1701c
Gabriel Freire <gabrbfreire@gmail.com>
2022-02-21 12:25:55 -0300

    first commit

---
+++ LinkShortener/Controllers/LinkController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class LinkController : ControllerBase
+    {
+        private readonly ILogger<LinkController> _logger;
+
+        public LinkController(ILogger<LinkController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult ShortenLink()
+        {
+            return Ok("лол");
+        }
+    }
+}
